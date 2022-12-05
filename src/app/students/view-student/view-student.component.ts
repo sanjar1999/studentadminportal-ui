60c622665a9e8061d28a99231268f62428bad60d
sanjar1999 <sanjar.work@gmail.com>
2022-12-05 16:19:38 +0500

    end of section 6

--- src/app/students/view-student/view-student.component.ts
+++ src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
 import { GenderService } from 'src/app/services/gender.service';
@@ -38,7 +38,7 @@ export class ViewStudentComponent implements OnInit {
 
   constructor(private readonly studentService: StudentService,
     private readonly route: ActivatedRoute, private genderService: GenderService,
-    private snackBar: MatSnackBar){}
+    private snackBar: MatSnackBar, private router: Router){}
 
 
   ngOnInit(): void {
@@ -70,5 +70,18 @@ export class ViewStudentComponent implements OnInit {
       },
       error: err => err
     });
+  }
+
+  onDelete(): void{
+    this.studentService.deleteStudent(this.student.id).subscribe({
+      next: (success) => {
+        this.snackBar.open('Student deleted successfully', undefined, {duration: 2000});
+
+        setTimeout(() => {
+          this.router.navigateByUrl('students');
+        }, 2000);
+      },
+      error: err => err
+    })
   }
 }
